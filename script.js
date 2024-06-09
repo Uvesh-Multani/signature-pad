55df4788ae2a61b5166cf54a510434f0c2ab217c
Uvesh Multani <143529701+Uvesh-Multani@users.noreply.github.com>
2024-06-09 14:33:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var downloadLink = document.getElementById('download-link');
     var colorPicker = document.getElementById('color-picker');
     var markerSizeInput = document.getElementById('marker-size');
+    var formatSelect = document.getElementById('format-select');
 
     // Set up canvas
     context.lineCap = 'round';
     context.lineJoin = 'round';
-    
+
     // Ensure the canvas dimensions match its display size
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
@@ -67,9 +68,40 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function saveSignature() {
-        var signatureData = canvas.toDataURL(); // Save signature as base64 data
+        var format = formatSelect.value;
+
+        if (format === 'image/jpeg' || format === 'image/jpg') {
+            // Set background to white for JPEG/JPG
+            var tempCanvas = document.createElement('canvas');
+            var tempContext = tempCanvas.getContext('2d');
+            tempCanvas.width = canvas.width;
+            tempCanvas.height = canvas.height;
+
+            tempContext.fillStyle = '#FFFFFF';
+            tempContext.fillRect(0, 0, canvas.width, canvas.height);
+            tempContext.drawImage(canvas, 0, 0);
+
+            var signatureData = tempCanvas.toDataURL(format);
+        } else if (format === 'image/gif') {
+            gifshot.createGIF({
+                images: [canvas.toDataURL()],
+                gifWidth: canvas.width,
+                gifHeight: canvas.height,
+            }, function(obj) {
+                if (!obj.error) {
+                    var image = obj.image;
+                    downloadLink.href = image;
+                    downloadLink.download = 'signature.gif';
+                    downloadLink.click();
+                }
+            });
+            return; // Skip the rest of the function for GIF
+        } else {
+            var signatureData = canvas.toDataURL(format);
+        }
+
         downloadLink.href = signatureData;
-        downloadLink.download = 'signature.png';
+        downloadLink.download = 'signature.' + format.split('/')[1];
         downloadLink.click();
     }
 
